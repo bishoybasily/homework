271e5a6638b099cc4b84c766155601e0a19c6cab
bishoy basily <bishoybasily@gmail.com>
2019-05-01 11:22:26 +0200

    home screen init

--- java/src/main/java/com/gmail/bishoybasily/issr/proj/screen/Home.java
+++ java/src/main/java/com/gmail/bishoybasily/issr/proj/screen/Home.java
@@ -1,20 +1,41 @@
 package com.gmail.bishoybasily.issr.proj.screen;
 
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Home implements Initializable, Screen {
 
-    public Parent load() throws Exception {
-        return FXMLLoader.load(getClass().getResource("/home.fxml"));
+    @FXML
+    private TableView tableUsers;
+    @FXML
+    private TableColumn columnName, columnEmail, columnTelephone;
+    @FXML
+    private TextField textName, textEmail, textTelephone;
+    @FXML
+    private Button buttonSave;
+
+    @Override
+    public String getResource() {
+        return "/home.fxml";
     }
 
     public void initialize(URL location, ResourceBundle resources) {
 
+        buttonSave.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+
+
+            }
+        });
     }
 
 }
--- java/src/main/java/com/gmail/bishoybasily/issr/proj/screen/Screen.java
+++ java/src/main/java/com/gmail/bishoybasily/issr/proj/screen/Screen.java
@@ -1,9 +1,14 @@
 package com.gmail.bishoybasily.issr.proj.screen;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
 public interface Screen {
 
-    Parent load() throws Exception;
+    default Parent load() throws Exception {
+        return FXMLLoader.load(getClass().getResource(getResource()));
+    }
+
+    String getResource();
 
 }
